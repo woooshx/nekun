7f181b288c526861b796c13871b2a6cb551c50c9
Wooosh <josephkunx@gmail.com>
2025-03-16 23:10:24 -0500

    pruebas sumitas y Copilot

---
+++ hola.py
@@ -0,0 +1,9 @@
+# Solicitar al usuario que ingrese dos números enteros
+num1 = int(input("Ingrese el primer número entero: "))
+num2 = int(input("Ingrese el segundo número entero: "))
+
+# Sumar los dos números
+suma = num1 + num2
+
+# Mostrar el resultado
+print("La suma de los dos números es:", suma)
